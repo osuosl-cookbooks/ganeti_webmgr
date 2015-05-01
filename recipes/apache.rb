eb7f94e3b8734786f9b01ede5f128a5ada9c736b
Evan Tschuy <tschuy@osuosl.org>
2015-05-01 12:21:48 -0700

    rubocop fixes

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -1,28 +1,29 @@
-include_recipe "apache2::default"
-include_recipe "apache2::mod_wsgi"
+include_recipe 'apache2::default'
+include_recipe 'apache2::mod_wsgi'
 
 gwm = node['ganeti_webmgr']
 
 env = {
-  "GWM_CONFIG_DIR" => gwm['config_dir'].to_s,
-  "DJANGO_SETTINGS_MODULE" => "ganeti_webmgr.ganeti_web.settings"
+  'GWM_CONFIG_DIR' => gwm['config_dir'].to_s,
+  'DJANGO_SETTINGS_MODULE' => 'ganeti_webmgr.ganeti_web.settings'
 }
 
 python_version = node['languages']['python']['version']
 # virtualenvs only have the major version
 # (ie: 2.6, not 2.6.6)
-version = python_version.split(".")
-version = version[0] + "." + version[1]
+version = python_version.split('.')
+version = version[0] + '.' + version[1]
 python_version = "python#{version}"
 
-python_path = ::File.join(gwm['install_dir'], 'lib', python_version, 'site-packages')
+python_path = ::File.join(
+  gwm['install_dir'], 'lib', python_version, 'site-packages')
 wsgi_path = ::File.join(python_path, 'ganeti_webmgr', 'ganeti_web', 'wsgi.py')
 
 venv = gwm['install_dir']
 venv_bin = ::File.join(venv, 'bin')
 django_admin = ::File.join(venv_bin, 'django-admin.py')
 
-execute "collect_static" do
+execute 'collect_static' do
   command "#{django_admin} collectstatic --noinput"
   environment env
   user node['ganeti_webmgr']['user']
