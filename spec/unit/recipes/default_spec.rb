b77d8e44ce1b0b2d48d3013d4bb971371ff24dbd
fahlmant <fahlmant@osuosl.org>
2017-03-31 15:49:40 -0700

    Stub encrypted databag item

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,6 +6,17 @@ describe 'ganeti_webmgr::default' do
       cached(:chef_run) do
         ChefSpec::SoloRunner.new(p).converge(described_recipe)
       end
+      before do
+        allow(Chef::EncryptedDataBagItem).to receive(:load).with('ganeti_webmgr', 'passwords').and_return(
+          id: 'passwords',
+          db_password: 'vagrant',
+          secret_key: 'asdf',
+          web_mgr_api_key: '12345',
+          db_server: {
+            'password' => 'rootpass'
+          }
+        )
+      end
       it 'converges successfully' do
         expect { chef_run }.to_not raise_error
       end
