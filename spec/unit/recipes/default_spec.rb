6e0baa6ef94eaa7ad0cf6a4bf49f041e1848d225
codymalick <cody.malick@gmail.com>
2017-04-04 11:25:15 -0700

    Removed debian from test cases, added more rspec tests for resources

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -20,11 +20,41 @@ describe 'ganeti_webmgr::default' do
       it 'converges successfully' do
         expect { chef_run }.to_not raise_error
       end
-      it do
+      it 'creates the application directory' do
         expect(chef_run).to create_directory(
           '/opt/ganeti_webmgr_src'
         ).with(
           owner: nil,
+          group: nil
+        )
+      end
+      it 'clones the gwm git repo' do
+        expect(chef_run).to sync_git(
+          '/opt/ganeti_webmgr_src'
+        ).with(
+          repository: 'https://github.com/osuosl/ganeti_webmgr'
+        )
+      end
+      it 'prints out a log' do
+        expect(chef_run).to write_log(
+          'Installing additional system packages for Ganeti Web Manager'
+        )
+      end
+      it 'installs required packages' do
+        expect(chef_run).to install_package(
+          'libffi-devel'
+        )
+        expect(chef_run).to install_package(
+          'openssl-devel'
+        )
+      end
+      it 'runs the ./scripts/setup.sh script' do
+        expect(chef_run).to run_execute(
+          'install_gwm'
+        ).with(
+          command: './scripts/setup.sh -D sqlite -d /opt/ganeti_webmgr',
+          cwd: '/opt/ganeti_webmgr_src',
+          user: nil,
           group: nil
         )
       end
