21063a364a35718d0d59ed60140ca038d11943da
Lance Albertson <lance@osuosl.org>
2018-11-15 11:26:45 -0800

    Add basic ChefSpec (doesn't pass yet)

---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+ChefSpec::Coverage.start! { add_filter 'ganeti_webmgr' }
+
+CENTOS_6 = {
+  platform: 'centos',
+  version: '6.9',
+}.freeze
+
+ALL_PLATFORMS = [
+  CENTOS_6,
+].freeze
+
+RSpec.configure do |config|
+  config.log_level = :fatal
+end
+
+shared_context 'common' do
+  before do
+    stub_data_bag_item('ganeti_webmgr', 'passwords')
+      .and_return(
+        db_password: 'db_password',
+        secret_key: 'secret_key',
+        web_mgr_api_key: 'web_mgr_api_key',
+        db_server: 'db_server'
+      )
+  end
+end
