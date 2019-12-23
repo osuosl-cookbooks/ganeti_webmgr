e6179ae2043f14e3bb3a028ddfb2a0a361081f2d
Robert Detjens <detjensrobert@osuosl.org>
2019-12-22 16:27:23 -0800

    remove now-native dependencies

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 
 depends          'apache2', '~> 4.0.0'
-depends          'build-essential'
 depends          'chef_nginx'
 depends          'git'
 depends          'hostsfile'
